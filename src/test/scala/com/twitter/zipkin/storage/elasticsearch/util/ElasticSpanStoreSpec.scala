0b389d1d8db09f5817d2406cc319fc564cd040fe
Jed Wesley-Smith <jed@atlassian.com>
2016-02-10 16:51:05 +1100

    now properly runs the scalatest specs, first cut impl of the SpanStoreSpec (not yet working)

--- src/test/scala/com/twitter/zipkin/storage/elasticsearch/util/ElasticSpanStoreSpec.scala
+++ src/test/scala/com/twitter/zipkin/storage/elasticsearch/util/ElasticSpanStoreSpec.scala
@@ -1,8 +1,56 @@
-package com.twitter.zipkin.storage
-package elasticsearch.util
+package com.twitter.zipkin
+package storage
+package elasticsearch
+package util
+
+import java.util.concurrent.TimeUnit
+
+import com.twitter.finagle.stats.{Counter, Gauge, Stat, StatsReceiver}
+import com.twitter.util.{Duration, FuturePool, Timer}
 
 class ElasticSpanStoreSpec extends SpanStoreSpec {
 
-  def clear: Unit = ???
-  def store: com.twitter.zipkin.storage.SpanStore = ???
+  def clear: Unit = ()
+
+  def store: SpanStore =
+    new ElasticSpanStore(
+      db = new DB(
+        Config(
+          host = "localhost"
+          , portOption = None
+          , clientTransportSniff = false
+          , clusterName = "nocluster"
+          , shield = None: Option[ShieldConfig]
+        )
+        , CircuitBreakerConfig(
+          executor = FuturePool.immediatePool
+          , timer = Timer.Nil
+          , maxFailures = 0
+          , callTimeout = Duration(1, TimeUnit.SECONDS)
+          , resetTimeout = Duration(1, TimeUnit.SECONDS)
+        )
+        , stats = NullStatsReceiver
+      )
+      , patterns = List()
+      , timer = Timer.Nil
+    )
+}
+
+object NullStatsReceiver extends StatsReceiver {
+  override val repr: AnyRef = this.getClass.getSimpleName
+
+  override def counter(name: String*): Counter =
+    new Counter {
+      override def incr(delta: Int): Unit = ()
+    }
+
+  override def addGauge(name: String*)(f: => Float): Gauge =
+    new Gauge {
+      override def remove(): Unit = ()
+    }
+
+  override def stat(name: String*): Stat =
+    new Stat {
+      override def add(value: Float): Unit = ()
+    }
 }
